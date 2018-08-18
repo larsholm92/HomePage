d5db5517024cc554b701521df45b41f211ab497c
Lars Holm <larsholm92@gmail.com>
2018-08-18 11:58:02 +0200

    First commit

--- sondergaardholm/Controllers/HomeController.cs
+++ sondergaardholm/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace sondergaardholm.Controllers
 {
@@ -13,17 +14,8 @@ namespace sondergaardholm.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult CleaningScheme()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
