df89d7b7ae5ff8bc31de1dec4bb5d83be031a57e
Lars Holm <larsholm92@gmail.com>
2019-02-19 17:50:17 +0100

    Todo

---
+++ sondergaardholm/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace sondergaardholm.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: Todo
+        public  async Task<ActionResult> Index()
+        {   
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("http://192.168.1.35:5001");
+                }
+                catch(Exception e)
+                {
+                    return View();
+                }                                  
+                
+            }
+                    
+            return Redirect("http://192.168.1.35:5001");
+        }
+    }
+}
